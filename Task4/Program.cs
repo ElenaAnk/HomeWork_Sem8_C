431e36c0cce6e2bbf8fba66b6b6eaf28ecc0e21a
Elena <elena_nn@list.ru>
2023-07-07 16:27:56 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+
+int[,,] newarray = GetArray3x(2, 2, 2, 10, 99);
+PrintArray3x(newarray);
+
+int[,,] GetArray3x(int row, int col, int z, int minValue, int maxValue)
+{
+    int[,,] result = new int[row, col, z];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                result[i, j, k] = new Random().Next(minValue, maxValue + 1);
+            }
+
+        }
+    }
+    return result;
+}
+
+void PrintArray3x(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.Write($"{inArray[i, j, k]} {(i, j, k)}  ");
+            }
+            Console.WriteLine();
+        }
+     
+    }
+}
+
+
+
