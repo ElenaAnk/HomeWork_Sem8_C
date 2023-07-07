9f95d32ae7b261a445cf22558bdae6d033166eaf
Elena <elena_nn@list.ru>
2023-07-07 16:27:36 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+1, 2, 3
+
+4, 6, 1
+
+2, 1, 6
+
+1 встречается 3 раза
+
+2 встречается 2 раз
+
+3 встречается 1 раз
+
+4 встречается 1 раз
+
+6 встречается 2 раза*/
+
+int[,] newarray = GetArray(3, 3, 1, 9);
+PrintArray(newarray);
+Console.WriteLine();
+GetCount(newarray);
+
+
+int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+    int[,] result = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetCount(int[,] Array)
+{
+    int Max = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            if (Array[i, j] > Max) Max = Array[i, j];
+        }
+    }
+
+    for (int y = 0; y <= Max; y++)
+    {
+        int count = 0;
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+            for (int j = 0; j < Array.GetLength(1); j++)
+            {
+                if (y == Array[i, j]) count += 1;
+            }
+        }
+        Console.WriteLine($"{y} встречается {count} раз");
+    }
+}
+
